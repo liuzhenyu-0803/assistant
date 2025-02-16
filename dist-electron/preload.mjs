b95320a10c792133e2dd8cb718239fb4f92b3b23
liuzhenyu <liuzhenyu0803@outlook.com>
2025-02-16 22:13:56 +0800

    chore: 从远程仓库移除 dist-electron 目录

--- dist-electron/preload.mjs
+++
@@ -1,6 +0,0 @@
-"use strict";
-const electron = require("electron");
-electron.contextBridge.exposeInMainWorld("electronAPI", {
-  readConfig: (configPath) => electron.ipcRenderer.invoke("read-config", configPath),
-  writeConfig: (configPath, data) => electron.ipcRenderer.invoke("write-config", configPath, data)
-});
